9bbf3167013a2a1743dbb79f7fed681ed4fa9a31
Oksana Vasyliuk <streranoksana@gmail.com>
2023-10-18 21:25:49 +0300

    homework

---
+++ main.py
@@ -0,0 +1,56 @@
+# def get_lit_sum (numbers):
+#      numbers_sum = 0
+#
+#      for num in numbers:
+#          numbers_sum += num
+#      return numbers_sum
+#
+# my_numbers = [n for n in range(6)]
+# print (my_numbers)
+# result =  get_lit_sum (my_numbers)
+# print(result)
+
+
+# def delete_number(list, number):
+#
+#    count = 0
+#
+#    for i in list:
+#
+#        if i == number:
+#
+#            list.remove(i)
+#
+#            count += 1
+#
+#    return count
+#
+# def combine_lists(list1, list2):
+#
+#  combined = list1 + list2
+#
+#  return combined
+#
+# list1 = [1, 2, 3]
+#
+# list2 = [4, 5, 6]
+#
+# combined_list = combine_lists(list1, list2)
+#
+# print(combined_list)
+
+# def find_min(mass):
+#
+#    elem_min = mass[0]
+#
+#    for elem in mass:
+#
+#        if elem < elem_min:
+#
+#            elem_min = elem
+#
+#    return elem_min
+#
+# mass = [int(s) for s in input().split()]
+#
+# print(find_min(mass))
